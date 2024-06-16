0126260692b6947ebbd0b79f3f30b4d98c1bd0db
GulievaDurush <quliyeva.d2005@gmail.com>
2024-06-16 21:58:03 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var name=document.getElementById("name");
+console.log("name");
+
+let yazi=document.getElementById("name");
+yazi="surname";
+console.log(yazi);
+
+const text=document.getElementById("name");
+text="first-name";
+console.log(text);
